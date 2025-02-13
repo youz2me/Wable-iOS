a4929ba52d2c9a4861b6c21067d78c3982f75563
youz2me <kynhun20@gachon.ac.kr>
2025-02-13 11:48:55 +0900

    Init: 프로젝트 생성

---
+++ Wable-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wable-iOS
+//
+//  Created by YOUJIM on 2/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
