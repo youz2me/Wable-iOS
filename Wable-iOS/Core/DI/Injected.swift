9e8c60359d2d0b314b2b586823e5fc9cac25aee6
JinUng41 <ingjwjw@naver.com>
2025-02-15 21:25:46 +0900

    [Feat] #13 - Injected 코드 구현

--- Wable-iOS/Core/DI/Injected.swift
+++ Wable-iOS/Core/DI/Injected.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct Injected<T> {
+    private var dependency: T
+    
+    init() {
+        self.dependency = AppDIContainer.shared.resolve(for: T.self)
+    }
+    
+    var wrappedValue: T {
+        get { dependency }
+    }
+}
